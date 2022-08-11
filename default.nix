770c38aea7886738a3c12a75508d50ab50553da9
Lana Black <lana@amok.cc>
2022-08-11 17:19:26 +0000

    solo5

--- default.nix
+++ default.nix
@@ -5,4 +5,7 @@ let
 in
 {
   appmenu-gtk3-module = callPackage ./packages/appmenu-gtk3-module { inherit (super); };
+  wrapAppmenuHook = callPackage ./hooks/wrapAppmenuHook { inherit (super); };
+
+  solo5 = callPackage ./packages/solo5 { inherit (super); };
 }
