690558a2b346a89934070f7ccfa123aa9d7c94c0
Lana Black <lanablack@amok.cc>
2022-11-28 06:01:32 +0000

    Fix ListLike for ghc925.

--- default.nix
+++ default.nix
@@ -2,7 +2,15 @@ self: super:
 
 let
   inherit (super) callPackage;
+  haskellOverlay = hself: hsuper: {
+    ListLike = super.haskell.lib.dontCheck hsuper.ListLike;
+  };
 in
 {
   appmenu-gtk3-module = callPackage ./packages/appmenu-gtk3-module { inherit (super); };
+  haskell = super.haskell // {
+    packages = super.haskell.packages // {
+      ghc925 = super.haskell.packages.ghc925.extend haskellOverlay;
+    };
+  };
 }
