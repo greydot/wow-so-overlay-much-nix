2173adb8de88d61f73110df604c7c856c1ff40f0
Lana Black <lanablack@amok.cc>
2023-02-13 11:35:35 +0000

    Another haskell package.

--- default.nix
+++ default.nix
@@ -4,13 +4,14 @@ let
   inherit (super) callPackage;
   haskellOverlay = hself: hsuper: {
     ListLike = super.haskell.lib.dontCheck hsuper.ListLike;
+    hls-brittany-plugin = super.haskell.lib.dontCheck hsuper.hls-brittany-plugin;
   };
 in
 {
   appmenu-gtk3-module = callPackage ./packages/appmenu-gtk3-module { inherit (super); };
-#  haskell = super.haskell // {
-#    packages = super.haskell.packages // {
-#      ghc925 = super.haskell.packages.ghc925.extend haskellOverlay;
-#    };
-#  };
+  haskell = super.haskell // {
+    packages = super.haskell.packages // {
+      ghc925 = super.haskell.packages.ghc925.extend haskellOverlay;
+    };
+  };
 }
