f11b0d788dd409a21ea28b1f1f0ca63fb1ee85df
Lana Black <lanablack@amok.cc>
2023-02-17 13:48:44 +0000

    Another haskell plugin.

--- default.nix
+++ default.nix
@@ -5,12 +5,14 @@ let
   haskellOverlay = hself: hsuper: {
     ListLike = super.haskell.lib.dontCheck hsuper.ListLike;
     hls-brittany-plugin = super.haskell.lib.dontCheck hsuper.hls-brittany-plugin;
+    hls-fourmolu-plugin = super.haskell.lib.dontCheck hsuper.hls-fourmolu-plugin;
   };
 in
 {
   appmenu-gtk3-module = callPackage ./packages/appmenu-gtk3-module { inherit (super); };
   haskell = super.haskell // {
     packages = super.haskell.packages // {
+      ghc902 = super.haskell.packages.ghc902.extend haskellOverlay;
       ghc925 = super.haskell.packages.ghc925.extend haskellOverlay;
     };
   };
